ba0ee2fd2a11cbf3c295aec3b461f9f9fe4ec128
thmsn <9084377+thmsndk@users.noreply.github.com>
2021-05-07 21:31:36 +0200

    supress a warning in rollup marking `game` and `arena` external

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 "use strict";
 
-import clear from 'rollup-plugin-clear';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from 'rollup-plugin-typescript2';
-import screeps from 'rollup-plugin-screeps';
+import clear from "rollup-plugin-clear";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "rollup-plugin-typescript2";
+import screeps from "rollup-plugin-screeps";
 
 let cfg;
 const dest = process.env.DEST;
@@ -16,6 +16,7 @@ if (!dest) {
 
 export default {
   input: "src/alpha-capture_the_flag/main.ts",
+  external: ["game", "arena"], // <-- suppresses the warning
   output: {
     file: "dist/alpha-capture_the_flag/main.mjs",
     format: "es",
@@ -26,7 +27,7 @@ export default {
     clear({ targets: ["dist"] }),
     resolve({ rootDir: "src" }),
     commonjs(),
-    typescript({tsconfig: "./tsconfig.json"}),
+    typescript({ tsconfig: "./tsconfig.json" })
     // screeps({config: cfg, dryRun: cfg == null})
   ]
-}
+};
