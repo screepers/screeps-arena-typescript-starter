2ce7500ae57a256e59e3266d1387bc06181cba02
Skyler Kehren <pyrodogg@gmail.com>
2021-05-08 12:10:42 +0300

    Add descriptive comment to util module

---
+++ src/utils/index.ts
@@ -0,0 +1,14 @@
+export { ErrorMapper } from "./ErrorMapper";
+
+/*
+  This file `index.ts` defines the `utils` module (since it's in the `utils` folder)
+
+  You can add additional functionality to the `utils` by "re-exporting" other modules in the folder (see ErrorMapper above.)
+
+  This allows you to flexibly import utilities in other modules
+
+  import { ErrorMapper } from "utils";
+  or
+  import { ErrorMapper } from "utils/ErrorMapper"
+
+*/
