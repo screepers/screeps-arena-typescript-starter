19ef788373bf144b88c095ab6d70aee7227d7c19
admon84 <admon84@users.noreply.github.com>
2021-05-16 07:12:29 -0600

    Fix: RoomObject methods (#5)
    * Fixing new methods in the RoomObject class

--- @types/game/prototypes/room-object.d.ts
+++ @types/game/prototypes/room-object.d.ts
@@ -19,36 +19,39 @@ declare module "game/prototypes" {
     exists: boolean;
 
     /**
+     * Get linear range to another position. pos may be any object containing x and y properties.
+     * @param pos
+     */
+    getRangeTo(pos: RoomPosition): number;
+
+    /**
      * Returns the path from this object to another position. pos can be any object containing x and y properties. See /game/utils::findPath for details.
      * @param pos
      * @param opts
      */
-    findPathTo(pos: RoomPosition, opts: FindPathOpts): PathStep[];
+    findPathTo(pos: RoomPosition, opts?: FindPathOpts): PathStep[];
 
     /**
      * Find all positions from the given positions array within the specified linear range.
-     * @param fromPos
      * @param positions
      * @param range
      */
-    findInRange(fromPos: RoomPosition, positions: RoomPosition[], range: number): RoomPosition[];
+    findInRange(positions: RoomPosition[], range: number): RoomPosition[];
 
     /**
      * Find a position with the shortest linear distance from the given position, or null otherwise.
-     * @param fromPos
      * @param positions
      */
-    findClosestByRange(fromPos: RoomPosition, positions: RoomPosition[]): RoomPosition;
+    findClosestByRange(positions: RoomPosition[]): RoomPosition;
 
     /**
      * Find a position with the shortest path from the given position, or null otherwise.
-     * @param fromPos
      * @param positions
      * @param opts object containing additional options:
      * ignore: array (objects which should be treated as obstacles during the search)
      * Any options supported by searchPath method
      */
-    findClosestByPath(fromPos: RoomPosition, positions: RoomPosition[], opts?: FindPathOpts): RoomPosition;
+    findClosestByPath(positions: RoomPosition[], opts?: FindPathOpts): RoomPosition;
 
     toJSON(): RoomObjectJSON;
   }
