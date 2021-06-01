47d4ee2c852b36dc6a8edef775362599e65cf648
thmsn <9084377+thmsndk@users.noreply.github.com>
2021-06-01 22:49:25 +0200

    spawnCreep currently returns a creep or an errorcode

--- @types/game/prototypes/structure-spawn.d.ts
+++ @types/game/prototypes/structure-spawn.d.ts
@@ -1,5 +1,11 @@
 declare module "game/prototypes" {
-  import { BodyPartConstant, ResourceConstant } from "game/constants";
+  import {
+    BodyPartConstant,
+    ERR_BUSY,
+    ERR_INVALID_ARGS,
+    ERR_NOT_ENOUGH_ENERGY,
+    ResourceConstant
+  } from "game/constants";
   import { Store } from "game/prototypes";
   export type STRUCTURE_SPAWN = "spawn";
   // export const STRUCTURE_SPAWN: STRUCTURE_SPAWN;
@@ -10,8 +16,9 @@ declare module "game/prototypes" {
     store: Store<ResourceConstant>;
     /**
      * Start the creep spawning process. The required energy amount can be withdrawn from all spawns and extensions in the room.
+     * @returns A creep on success or an errorcode on failure
      */
-    spawnCreep(body: BodyPartConstant[]): Creep;
+    spawnCreep(body: BodyPartConstant[]): Creep | ERR_BUSY | ERR_INVALID_ARGS | ERR_NOT_ENOUGH_ENERGY;
   }
   interface StructureSpawnConstructor extends _Constructor<StructureSpawn>, _ConstructorById<StructureSpawn> {}
 
