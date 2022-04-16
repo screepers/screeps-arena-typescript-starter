7aeda6c795b18a3324e8359e6a4a40a51bbe3352
somq14 <som.q14@gmail.com>
2022-04-16 19:00:57 +0900

    ⬆️ Update prettier and related packages
    No need to extend "prettier/@typescript-eslint" since eslint-config-prettier@8.0.0
    cf. https://github.com/prettier/eslint-config-prettier/blob/main/CHANGELOG.md#version-800-2021-02-21

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,6 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript"
